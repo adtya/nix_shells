fde440b1556e0910f1e5f204d5803524007ff44b
Adithya Nair <adtya@adtya.xyz>
2022-06-30 08:36:06 +0530

    update ruby shell

--- ruby/shell.nix
+++ ruby/shell.nix
@@ -3,10 +3,16 @@
 pkgs.mkShell {
 	buildInputs = [
 		pkgs.ruby_3_1
+		pkgs.sqlite
+		pkgs.postgresql
+		pkgs.libpcap
+		pkgs.libxml2
+		pkgs.libxslt
+		pkgs.pkg-config
 	];
 	shellHook = ''
 		export GEM_HOME=$PWD/.nix/gems
 		export GEM_PATH=$GEM_HOME:$GEM_PATH
-		export PATH=$PWD/bin:$GEM_HOME/bin:$PATH
+		export PATH=$GEM_HOME/bin:$PATH
 	'';
 }
